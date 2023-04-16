7ff5868341fe0e4957dd2a9f919cdb0a88fb1113
현수 조 <jhsroid@naver.com>
2023-04-17 00:55:51 +0900

    feat : 맵 구조와 방향키를 통한 자리바꾸기 추가
    맵과 자리바꾸기 기능 은 완성하고 게임 클리어 확인 함수까지 구현하였지만 클리어함수는 아직 미완성

--- SlidePuzzle/Program.cs
+++ SlidePuzzle/Program.cs
@@ -5,8 +5,9 @@ namespace SlidePuzzle
     internal class Program
     {
         enum Command { None, Up, Down, Left, Right }
+        public enum Tile { None, Wall }
 
-        public class Point
+        public struct Point
         {
             private int x;
             private int y;
@@ -27,31 +28,101 @@ namespace SlidePuzzle
             }
         }
 
-        public class Map
+        public class Stage
         {
             public Point player;
-            private int[,] map;
-            public Map(int[,] map)
+            public Tile[,] map5 = new Tile[13, 13]
             {
-                this.map = map;
+                { Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.None, Tile.Wall },
+                { Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall, Tile.Wall }
+            };
+            public int[,] answer;
+            public int[,] number;
+            public Stage(Point player)
+            {
+                this.player = player;
+            }
+
+
+            public int LengthX
+            {
+                get { return (map5.GetLength(1)); }
+            }
+            public int LengthY
+            {
+                get { return (map5.GetLength(0)); }
             }
         }
 
         static void Main(string[] args)
         {
             // 게임 초기화
-            Point player = new Point(4, 4);
-            Map map = new Map(new int[5, 5]);
+            Point player = new Point(10, 10);
+            Stage stage = new Stage(player);
+            stage.number = new int[stage.map5.GetLength(1), stage.map5.GetLength(0)];
+            stage.answer = new int[stage.map5.GetLength(1), stage.map5.GetLength(0)];
+            Console.CursorVisible = false;
+            SetGame(stage);
 
+            Rendering(stage);
             while (true)
             {
                 // Input
                 Command input = GameInput();
 
                 // GameUpdate
-                GameUpdate(input, map);
+                GameUpdate(input, stage);
 
                 // Rendering
+                Rendering(stage);
+
+                // 클래이 확인
+                bool clear = CheckClear(stage);
+                if (clear) { break; }
+            }
+        }
+
+        // SetGame
+        static void SetGame(Stage stage)
+        {
+            Random random5 = new Random();
+            int[] rand = new int[24];
+            for (int i = 0; i < 24; i++)
+            {
+                rand[i] = i + 1;
+                stage.answer[2 * ((i / 5) + 1), 2 * ((i % 5) + 1)] = rand[i];
+            }/*
+            for (int a=0; a<13; a++)
+            {
+                Console.Write("{");
+                for (int b=0; b<13; b++)
+                {
+
+                    if (stage.answer[a, b] < 10)
+                    {
+                        
+                        Console.Write("  {0}", stage.answer[a, b]);
+                    }
+                    else { Console.Write(" {0}", stage.answer[a,b]); }
+                }
+                Console.Write(" }");
+                Console.WriteLine();
+            }*/
+            rand = rand.OrderBy(x => random5.Next()).ToArray();
+            for (int j = 0; j < 24; j++)
+            {
+                stage.number[2 * ((j / 5) + 1), 2 * ((j % 5) + 1)] = rand[j];
             }
         }
 
@@ -60,7 +131,7 @@ namespace SlidePuzzle
         {
             Command command;
             ConsoleKeyInfo key = Console.ReadKey();
-            switch(key.Key)
+            switch (key.Key)
             {
                 case ConsoleKey.W:
                 case ConsoleKey.UpArrow:
@@ -86,33 +157,96 @@ namespace SlidePuzzle
         }
 
         // GameUpdate
-        static void GameUpdate(Command input, Map map)
+        static void GameUpdate(Command input, Stage stage)
         {
-            Point prevPoint = map.player;
-            switch(input)
+            Point prevPoint = stage.player;
+            switch (input)
             {
                 case Command.Up:
-                    map.player.Y--;
+                    stage.player.Y -= 2;
                     break;
                 case Command.Down:
-                    map.player.Y++;
+                    stage.player.Y += 2;
                     break;
                 case Command.Left:
-                    map.player.X--;
+                    stage.player.X -= 2;
                     break;
                 case Command.Right:
-                    map.player.X++;
+                    stage.player.X += 2;
                     break;
                 default:
-                    map.player = prevPoint;
+                    stage.player = prevPoint;
                     break;
+            }
+
+            if (stage.map5[stage.player.Y, stage.player.X] == Tile.Wall)
+            {
+                stage.player = prevPoint;
+            }
+            else
+            {
+                switch (input)
+                {
+                    case Command.Up:
+                        stage.number[(stage.player.Y + 2), stage.player.X] = stage.number[stage.player.Y, stage.player.X];
+                        stage.number[stage.player.Y, stage.player.X] = 0;
+                        break;
+                    case Command.Down:
+                        stage.number[(stage.player.Y - 2), stage.player.X] = stage.number[stage.player.Y, stage.player.X];
+                        stage.number[stage.player.Y, stage.player.X] = 0;
+                        break;
+                    case Command.Left:
+                        stage.number[stage.player.Y, (stage.player.X + 2)] = stage.number[stage.player.Y, stage.player.X];
+                        stage.number[stage.player.Y, stage.player.X] = 0;
+                        break;
+                    case Command.Right:
+                        stage.number[stage.player.Y, (stage.player.X - 2)] = stage.number[stage.player.Y, stage.player.X];
+                        stage.number[stage.player.Y, stage.player.X] = 0;
+                        break;
+                }
             }
         }
 
         // Rendering
-        static void Rendering(Map map)
+        static void Rendering(Stage stage)
         {
-            for (int y=0; y<map.GetLength(0);)
+            Console.Clear();
+            for (int y = 0; y < stage.LengthY; y++)
+            {
+                for (int x = 0; x < stage.LengthX; x++)
+                {
+                    switch (stage.map5[y, x])
+                    {
+                        case Tile.Wall:
+                            Console.Write("▩");
+                            break;
+                        case Tile.None:
+                            // 숫자 배치
+                            if (stage.number[y, x] != 0)
+                            {
+                                if (stage.number[y, x] < 10) { Console.Write(" {0}", stage.number[y, x]); }
+                                else { Console.Write(stage.number[y, x]); }
+                            }
+                            else { Console.Write("　"); }
+                            break;
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            // 플레이어 좌표
+            Console.SetCursorPosition(stage.player.X * 2, stage.player.Y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write('★');
+            Console.ResetColor();
+        }
+
+        // 클리어 확인
+        static bool CheckClear(Stage stage)
+        {
+            bool clear = false;
+            if (stage.number == stage.answer) { clear = true; }
+            return clear;
         }
     }
 }
